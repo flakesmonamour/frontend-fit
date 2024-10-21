7fb26dc5534d19a7c7094609e673a6e384745999
flakesmonamour <mwangimark456@gmail.com>
2024-10-21 16:22:41 +0300

    initial commit

---
+++ backend/models.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    type = db.Column(db.String(100))
+    duration = db.Column(db.Integer)
+    calories = db.Column(db.Integer)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    members = db.Column(db.Integer, default=0)
+
+class Goal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    description = db.Column(db.String(200))
+    target = db.Column(db.String(100))
+    frequency = db.Column(db.String(50))
+    completed = db.Column(db.Boolean, default=False)
